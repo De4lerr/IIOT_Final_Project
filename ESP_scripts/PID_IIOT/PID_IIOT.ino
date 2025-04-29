7b15b7cbf44c2bd7b02e68f6c6c5b34ebb068edf
De4lerr <dilermandoa7@gmail.com>
2025-04-28 21:20:51 -0300

    adding esp 32 code

--- ESP_scripts/PID_IIOT/PID_IIOT.ino
+++ ESP_scripts/PID_IIOT/PID_IIOT.ino
@@ -1,5 +1,13 @@
+#define ENC_A 22
+#define ENC_B 23
+#define PWM   18
+#define H_IN1 19
+#define H_IN2 21
+
+#include <Arduino.h>
+
 //Incluindo bibliot//Incluindo biblioteca para leitura do encoder
-#include <Encoder.h>
+#include <ESP32Encoder.h>
 
 /*-------------------------------COMUNICAÇÃO------------------------------------*/
                                                                                 //
@@ -10,16 +18,16 @@
                                                                                 //
 // Definindo objetos wifi e mqqt                                                //
 WiFiClient espClient;                                                           //
-PubSubClient MQTT(espClient);                                                   //
+PubSubClient client(espClient);                                                 //
                                                                                 //
 // Definindo variáveis para controle de wifi e mqtt                             //
 String status = "";                                                             //
-int count = 0;                                                                  //
+int count = 8;                                                                  //
                                                                                 //
 /*-------------------------------COMUNICAÇÃO------------------------------------*/
 
 //Definindo objeto meuEncoder
-Encoder meuEncoder(2, 3);
+ESP32Encoder meuEncoder;
 
 //Definindo outras variaveis uteis
 double theta = 0.0, tempo1 = 0.0, tempo2 = 0.0, dt = 0.0, erro = 0.0, u = 0.0;
@@ -29,28 +37,29 @@ long contEnc = 0.0;
 // Escolhendo referencia
 double thetaRef = 0.0; //em rad
 
-double ThetaPot = 0.0;   //_________________________________________________________BRENO
+double ThetaPot = 120.0*3.1415/180.0;   //__________________________________________TOPICO
 
 //Definindo ganho do controlador proporcional
-double Kp = 0.0;         //_________________________________________________________BRENO
-double Ti = 0.0;         //_________________________________________________________BRENO
-double Td = 0.0;         //_________________________________________________________BRENO
+double Kp = 0.0;         //_________________________________________________________TOPICO
+double Ti = 0.0;         //_________________________________________________________TOPICO
+double Td = 0.0;         //_________________________________________________________TOPICO
 
 //Definindo variaveis booleanas
-bool PID = false;        //_________________________________________________________BRENO
-bool ANTI_WINDUP = false;//_________________________________________________________BRENO
-bool THETA_POT = false;  //_________________________________________________________BRENO
+bool PID = false;        //_________________________________________________________TOPICO
+bool ANTI_WINDUP = false;//_________________________________________________________TOPICO
+bool THETA_POT = false;  //_________________________________________________________TOPICO
 
 void setup() {
   //Inicializando comunicacao serial  
   Serial.begin(115200);
+  meuEncoder.attachHalfQuad(ENC_A, ENC_B);
 
 /*-------------------------------COMUNICAÇÃO WIFI-------------------------------*/
                                                                                 //
   do {                                                                          //
     if(count) Serial.println("\n\nConnection error, trying again.\n");          //
                                                                                 //
-    count %= 3;                                                                 //
+    count %= 3;                                                               //
                                                                                 //
     switch(count) {                                                             //
       case 0:                                                                   //
@@ -61,8 +70,11 @@ void setup() {
         ewifi.setWiFi(SSID2, WPA2_AUTH_PEAP, anonymous, username, userpassword);//
         count++;                                                                //
         break;                                                                  //
+      case 2:                                                                   //
+        ewifi.setWiFi(SSID3,password);                                          //
+        count++;                                                                //
+        break;                                                                  //     
     }                                                                           //
-                                                                                //
     ewifi.connect();                                                            //
   } while(ewifi.status() != WL_CONNECTED);                                      //
                                                                                 //
@@ -71,24 +83,30 @@ void setup() {
 
 /*-------------------------------COMUNICAÇÃO MQTT-------------------------------*/
                                                                                 //
-  MQTT.setServer(MQTTServer, MQTTPort);                                         //
-  MQTT.setCallback(callback);                                                   //
+  client.setServer(MQTTServer, MQTTPort);                                       // 
+  client.setCallback(callback);                                                 // 
                                                                                 //
 /*-------------------------------COMUNICAÇÃO MQTT-------------------------------*/
 
-   //Definindo Entradas da ponte H
-  pinMode(5,OUTPUT); //velocidade de giro
-  pinMode(7,OUTPUT); //sentido de giro
-  pinMode(8,OUTPUT); //sentido de giro
+  //Definindo Entradas da ponte H
+  pinMode(PWM,OUTPUT); //velocidade de giro
+  pinMode(H_IN1,OUTPUT); //sentido de giro
+  pinMode(H_IN2,OUTPUT); //sentido de giro
 }
 
 // Loop principal
-void loop() {
-  if (!MQTT.connected()) conectarMQTT();
+void loop() {  
+  if (!client.connected()) conectarMQTT();
 
-  MQTT.loop();
+  client.loop();
 
-  run_PID();
+  // Publicar feedback do controle
+  //String feedback = "Setpoint: " + String(setpoint) + ", PV: " + String(process_variable) + ", Output: " + String(output);
+  //client.publish(topic_feedback, feedback.c_str());
+
+  //Serial.println("."); delay(2000);
+
+  run_PID();//_______________________________________________________________________FUNÇÃO PRINCIPAL
 }
 
 /*----------------------------------------FUNÇÕES MQTT--------------------------------------*/
@@ -107,84 +125,53 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   if (status != msg) {
     status = msg;
-    msg.toLowerCase();
 
     // Parse the received message for control values
-    if (msg.indexOf("pid") != -1) {
-      // Extracting PID parameters (Kp, Ti, Td) and boolean flags (PID, ANTI_WINDUP)
-      int kpIndex = msg.indexOf("Kp");
-      int tiIndex = msg.indexOf("Ti");
-      int tdIndex = msg.indexOf("Td");
-      int thetapotIndex = msg.indexOf("ThetaPot");
-      int pidIndex = msg.indexOf("PID");
-      int awIndex = msg.indexOf("ANTI_WINDUP");
-      int thetaIndex = msg.indexOf("THETA_POT");
+    Kp = extractFloatValue(msg, "KP", Kp);
+    Ti = extractFloatValue(msg, "TI", Ti);
+    Td = extractFloatValue(msg, "TD", Td);
+    ThetaPot = extractFloatValue(msg, "ThetaPot", ThetaPot);
+    PID = extractBoolValue(msg, "PID", PID);
+    ANTI_WINDUP = extractBoolValue(msg, "ANTI_WINDUP", ANTI_WINDUP);
+    THETA_POT = extractBoolValue(msg, "THETA_POT", THETA_POT);
 
-      // Example: msg = "Kp=2.0, Ti=1.5, Td=0.5, PID=true, ANTI_WINDUP=false"
-      if (kpIndex != -1) {
-        Kp = extractFloatValue(msg, kpIndex);
-        Serial.print("Novo valor de Kp: ");
-        Serial.println(Kp);
-      }
-
-      if (tiIndex != -1) {
-        Ti = extractFloatValue(msg, tiIndex);
-        Serial.print("Novo valor de Ti: ");
-        Serial.println(Ti);
-      }
-
-      if (tdIndex != -1) {
-        Td = extractFloatValue(msg, tdIndex);
-        Serial.print("Novo valor de Td: ");
-        Serial.println(Td);
-      }
-
-      if (thetapotIndex != -1) {
-        ThetaPot = extractFloatValue(msg, thetapotIndex);
-        Serial.print("Novo valor de ThetaPot: ");
-        Serial.println(ThetaPot);
-      }
-
-      if (pidIndex != -1) {
-        PID = extractBoolValue(msg, pidIndex);
-        Serial.print("Novo valor de PID: ");
-        Serial.println(PID ? "true" : "false");
-      }
-
-      if (awIndex != -1) {
-        ANTI_WINDUP = extractBoolValue(msg, awIndex);
-        Serial.print("Novo valor de ANTI_WINDUP: ");
-        Serial.println(ANTI_WINDUP ? "true" : "false");
-      }
-
-      if (thetaIndex != -1) {
-        THETA_POT = extractBoolValue(msg, thetaIndex);
-        Serial.print("Novo valor de THETA_POT: ");
-        Serial.println(THETA_POT ? "true" : "false");
-      }
-    }
+    Serial.print("Novo valor de Kp: ");
+    Serial.println(Kp);
+    Serial.print("Novo valor de Ti: ");
+    Serial.println(Ti);
+    Serial.print("Novo valor de Td: ");
+    Serial.println(Td);
+    Serial.print("Novo valor de ThetaPot: ");
+    Serial.println(ThetaPot);
+    Serial.print("Novo valor de PID: ");
+    Serial.println(PID ? "true" : "false");
+    Serial.print("Novo valor de ANTI_WINDUP: ");
+    Serial.println(ANTI_WINDUP ? "true" : "false");
+    Serial.print("Novo valor de THETA_POT: ");
+    Serial.println(THETA_POT ? "true" : "false");
   }
 }
 
 /* FUNCTION TO EXTRACT FLOAT VALUES FROM MESSAGE */
-float extractFloatValue(String msg, int index) {
-  String valueStr = "";
-  int valueStart = msg.indexOf('=', index) + 1;
-  int valueEnd = msg.indexOf(',', index);
-  if (valueEnd == -1) valueEnd = msg.length();
-  valueStr = msg.substring(valueStart, valueEnd);
-  return valueStr.toFloat();
+float extractFloatValue(const String& msg, const String& param, float origin) {
+  int startIdx = msg.indexOf(param + "=");
+  if (startIdx == -1) return origin;
+  startIdx += param.length() + 1;
+  int endIdx = msg.indexOf(",", startIdx);
+  if (endIdx == -1) endIdx = msg.length();
+  return msg.substring(startIdx, endIdx).toFloat();
 }
 
-/* FUNCTION TO EXTRACT BOOLEAN VALUES FROM MESSAGE */
-bool extractBoolValue(String msg, int index) {
-  String valueStr = "";
-  int valueStart = msg.indexOf('=', index) + 1;
-  int valueEnd = msg.indexOf(',', index);
-  if (valueEnd == -1) valueEnd = msg.length();
-  valueStr = msg.substring(valueStart, valueEnd);
-  return (valueStr == "true");
+bool extractBoolValue(const String& msg, const String& param, bool origin) {
+  int startIdx = msg.indexOf(param + "=");
+  if (startIdx == -1) return origin;
+  startIdx += param.length() + 1;
+  int endIdx = msg.indexOf(",", startIdx);
+  if (endIdx == -1) endIdx = msg.length();
+  String value = msg.substring(startIdx, endIdx);
+  return (value == "true");
 }
+
 
 /* MQTT FUNCTION FOR CONECT TO SERVER */
 void conectarMQTT() {
@@ -192,9 +179,16 @@ void conectarMQTT() {
   char esp_id[num];
 
   snprintf(esp_id, num, "ESP32-%s", ewifi.getmacAddress());
-  // printf("%s\n", esp_id);
-  while (!MQTT.connected())
-    if (MQTT.connect(esp_id)) MQTT.subscribe(topic);
+
+  Serial.println("ID do ESP:"); Serial.print(esp_id);
+
+  Serial.println("\nConectando ao Broker, aguarde...");
+
+  while (!client.connected())
+
+    if (client.connect(esp_id)) client.subscribe(topic);
+  
+  Serial.println("\nConexão com o Broker estabelecida!");
 }
 
 /*----------------------------------------FUNÇÃO PID--------------------------------------*/
@@ -205,17 +199,19 @@ void run_PID() {
   erroAnt = erro; 
 
   //Determinando leitura autal do encoder
-  contEnc = meuEncoder.read();
+  contEnc =  meuEncoder.getCount();
+
   
   //Calculando theta a partir da leitura do encoder
-  theta = contEnc*2.0*3.1415/(334.0*4.0); //em rad
+  theta = contEnc*3.1415/(334.0*4.0); //em rad
 
-  if (tempo2/1000000.0 > 3.0){
-    if(THETA_POT){
+  if (tempo2/1000000.0 > 20.0){
+    /*if(THETA_POT){
       thetaRef = ThetaPot;
     }else{
       thetaRef = 120.0*3.1415/180.0;
-    }
+    }*/
+    thetaRef = ThetaPot;
   }
 
   //Determinando tempo atual
@@ -240,19 +236,22 @@ void run_PID() {
   }
 
   //Calculando controle
-
-  u = PID * (Kp * (erro+1 / Ti*erroint + Td*erroderiv)) + !PID * (Kp*erro);
+  if(!PID){
+    u = Kp*erro;
+  }else{
+    u = Kp * (erro+1 / Ti*erroint + Td*erroderiv);
+  }
 
   //Alterando sentido de giro de acordo com o sinal do controle
   if (u >= 0){
         //sentido horario
-        digitalWrite(8,LOW);
-        digitalWrite(7,HIGH);
+        digitalWrite(H_IN2,LOW);
+        digitalWrite(H_IN1,HIGH);
   }
   else{
       //sentido anti-horario
-        digitalWrite(8,HIGH);
-        digitalWrite(7,LOW);
+        digitalWrite(H_IN2,HIGH);
+        digitalWrite(H_IN1,LOW);
         u = -u;
   }
     
--- ESP_scripts/PID_IIOT/WiFiPassword.h
+++ ESP_scripts/PID_IIOT/WiFiPassword.h
@@ -3,17 +3,21 @@
 */
 #define SSID1 "UFU-Institucional"
 #define SSID2 "eduroam"
-#define SSID3
-#define password
-#define username ""    // Email ufu
-#define userpassword ""           // Senha do email ufu
-#define anonymous ""   // Email ufu
+#define SSID3 ""
+#define password ""
+#define username "dilermandoa1@ufu.br"    // Email ufu
+#define userpassword "Atheon-7"           // Senha do email ufu
+#define anonymous "dilermandoa1@ufu.br"   // Email ufu
 
 /** Input WiFi data
  * Definition of macros SSID, username and password
 */
-#define MQTTServer "200.19.144.16"
+#define MQTTServer "10.14.108.205"//"192.168.100.37" //"200.19.144.16"
 #define MQTTPort 1883
 
 /* DECLARATION OF TOPICS VARIABLES */
 const char* topic = "/lab_pid";
+const char* topic_feedback = "lab_pid_feedback";
+
+// mosquitto_pub -h 192.168.100.37 -t "lab_pid_config" -m "Kp=2.0, Ti=1.5, Td=0.5, ThetaPot=3.14, PID=true, ANTI_WINDUP=false, THETA_POT=true"
+// mosquitto_sub -h localhost -t "lab_pid_config"
